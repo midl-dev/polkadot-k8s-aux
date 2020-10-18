8915a79343d90639abdb6d213d3c4a8fe7e8221c
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-10-18 15:08:49 -0700

    make payout script loop over several validators

--- docker/payout-cron/index.js
+++ docker/payout-cron/index.js
@@ -32,43 +32,46 @@ async function main () {
   ]);
 
 
-  const controller_address = await api.query.staking.bonded(process.env.STASH_ACCOUNT_ADDRESS);
-  const controller_ledger = await api.query.staking.ledger(controller_address.toString());
-  claimed_eras = controller_ledger.toHuman().claimedRewards.map(x => parseInt(x.replace(',','')));
-  console.log(`Payout for validator stash ${process.env.STASH_ACCOUNT_ADDRESS} has been claimed for eras: ${claimed_eras}`);
+  for (const stash_account of process.env.STASH_ACCOUNT_ADDRESS.split(",")) {
+    var controller_address = await api.query.staking.bonded(stash_account);
+    var controller_ledger = await api.query.staking.ledger(controller_address.toString());
+    claimed_eras = controller_ledger.toHuman().claimedRewards.map(x => parseInt(x.replace(',','')));
+    console.log(`Payout for validator stash ${stash_account} has been claimed for eras: ${claimed_eras}`);
 
-  if (claimed_eras.includes(currentEra - 1)) {
-    console.log(`Payout for validator stash ${process.env.STASH_ACCOUNT_ADDRESS} for era ${currentEra - 1} has already been issued, exiting`);
-    process.exit(0);
-  }
-
-  const exposure_for_era = await api.query.staking.erasStakers(currentEra - 1, process.env.STASH_ACCOUNT_ADDRESS);
-  if (exposure_for_era.total == 0) {
-    console.log(`Stash ${process.env.STASH_ACCOUNT_ADDRESS} was not in the active validator set for era ${currentEra - 1}, not payout can be made, exiting`);
-    process.exit(0);
-  }
-
-  console.log(`Issuing payoutStakers extrinsic from address ${process.env.PAYOUT_ACCOUNT_ADDRESS} for validator stash ${process.env.STASH_ACCOUNT_ADDRESS} for era ${currentEra - 1}`);
-
-  // Create, sign and send the payoutStakers extrinsic
-  const unsub = await api.tx.staking.payoutStakers(process.env.STASH_ACCOUNT_ADDRESS, currentEra - 1).signAndSend(payoutKey, ({ events = [], status }) => {
-    console.log('Transaction status:', status.type);
-
-    if (status.isInBlock) {
-      console.log('Included at block hash', status.asInBlock.toHex());
-      console.log('Events:');
-
-      events.forEach(({ event: { data, method, section }, phase }) => {
-        console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-      });
-    } else if (status.isFinalized) {
-      console.log('Finalized block hash', status.asFinalized.toHex());
-      process.exit(0);
-    } else if (status.isError) {
-      console.error('Errored out in block hash', status.asFinalized.toHex());
-      process.exit(1);
+    if (claimed_eras.includes(currentEra - 1)) {
+      console.log(`Payout for validator stash ${stash_account} for era ${currentEra - 1} has already been issued, exiting`);
+      continue;
     }
-  });
+
+    var exposure_for_era = await api.query.staking.erasStakers(currentEra - 1, stash_account);
+    if (exposure_for_era.total == 0) {
+      console.log(`Stash ${stash_account} was not in the active validator set for era ${currentEra - 1}, not payout can be made, exiting`);
+      continue;
+    }
+
+    console.log(`Issuing payoutStakers extrinsic from address ${process.env.PAYOUT_ACCOUNT_ADDRESS} for validator stash ${stash_account} for era ${currentEra - 1}`);
+
+    // Create, sign and send the payoutStakers extrinsic
+    var unsub = await api.tx.staking.payoutStakers(stash_account, currentEra - 1).signAndSend(payoutKey, ({ events = [], status }) => {
+      console.log('Transaction status:', status.type);
+
+      if (status.isInBlock) {
+        console.log('Included at block hash', status.asInBlock.toHex());
+        console.log('Events:');
+
+        events.forEach(({ event: { data, method, section }, phase }) => {
+          console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
+        });
+      } else if (status.isFinalized) {
+        console.log('Finalized block hash', status.asFinalized.toHex());
+      } else if (status.isError) {
+        console.error('Errored out in block hash', status.asFinalized.toHex());
+        process.exit(1);
+      }
+    });
+  }
+  console.log("Exiting");
+  process.exit(0);
 }
 
 main().catch(console.error);
