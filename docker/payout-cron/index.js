73ab888c08da95d8269a0cff3fc1dcf96f5b44ce
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-05-10 11:35:36 -0700

    add num_past_eras and verify more in the past

--- docker/payout-cron/index.js
+++ docker/payout-cron/index.js
@@ -19,6 +19,8 @@
  *  * NODE_ENDPOINT : the polkadot/kusama node rpc (localhost)
  *  * PAYOUT_ACOUNT_MNEMONIC: 12 words of the payout account (should have little balance, just for fees)
  *  * STASH_ACCOUNT_ADDRESS: the address of the validator's stash
+ *  * STASH_ACCOUNT_ALIAS: an alias for your validator
+ *  * NUM_PAST_ERAS: how many eras in the past to check for unpaid rewards
  *
  * The script queries the current era. It then verifies that:
  *
@@ -30,15 +32,17 @@
  *  If payout extrinsic fails, it will post an error to the console and to Slack
  *  if the SLACK_ALERT_TOKEN and SLACK_ALERT_CHANNEL env vars are set.
  *
- *  This script does not:
- *   * support multiple validators. To support multiple validator, run several cronjobs.
- *   * support older eras than the one before the current one. This script should run often.
+ *  This script does not support multiple validators. To support multiple validators, run several cronjobs.
+ *
+ *  Payout for multiple eras in the past is supported, but will throw an error.
+ *  This script is expected to run at least once per era.
  *
  *  To run once:
  *    export NODE_ENDPOINT=localhost
  *    export PAYOUT_ACCOUNT_MNEMONIC="your twelve key words..."
  *    export STASH_ACCOUNT_ADDRESS="GyrcqNwF87LFc4BRxhxakq8GZRVNzhGn3NLfSQhVHQxqYYx"
  *    export STASH_ACCOUNT_ALIAS="my awesome validator"
+ *    export NUM_PAST_ERAS=5
  *    node index.js
  *
  *  To run continously, put the following script in a cronjob.
@@ -68,58 +72,62 @@ async function main () {
 
   const stash_account = process.env.STASH_ACCOUNT_ADDRESS;
   const stash_alias = process.env.STASH_ACCOUNT_ALIAS; //optional
+  const num_past_eras = parseInt(process.env.NUM_PAST_ERAS);
   var controller_address = await api.query.staking.bonded(stash_account);
   var controller_ledger = await api.query.staking.ledger(controller_address.toString());
   claimed_eras = controller_ledger.toHuman().claimedRewards.map(x => parseInt(x.replace(',','')));
   console.log(`Payout for validator stash ${stash_account} has been claimed for eras: ${claimed_eras}`);
 
-  if (claimed_eras.includes(currentEra - 1)) {
-    console.log(`Payout for validator stash ${stash_account} for era ${currentEra - 1} has already been issued, exiting`);
-    process.exit(0);
-  }
+  for (i = 0; i < num_past_eras; i++) {
+    eraToClaim = currentEra - 1 - i;
 
-  var exposure_for_era = await api.query.staking.erasStakers(currentEra - 1, stash_account);
-  if (exposure_for_era.total == 0) {
-    console.log(`Stash ${stash_account} was not in the active validator set for era ${currentEra - 1}, no payout can be made, exiting`);
-    process.exit(0);
-  }
-
-  console.log(`Issuing payoutStakers extrinsic from address ${payoutKey.address} for validator stash ${stash_alias} (${stash_account}) for era ${currentEra - 1}`);
-
-  // Create, sign and send the payoutStakers extrinsic
-  try {
-    var unsub = await api.tx.staking.payoutStakers(stash_account, currentEra - 1).signAndSend(payoutKey, ({ events = [], status }) => {
-      console.log('Transaction status:', status.type);
-
-      if (status.isInBlock) {
-        console.log('Included at block hash', status.asInBlock.toHex());
-        console.log('Events:');
-
-        events.forEach(({ event: { data, method, section }, phase }) => {
-          console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-        });
-      } else if (status.isFinalized) {
-        console.log('Finalized block hash', status.asFinalized.toHex());
-      } else if (status.isError) {
-        var message = `Payout extrinsic was succesfully finalized on-chain but failed for validator ${stash_alias} (${stash_account}) with error ${status.asFinalized.toHex()}.`;
-        if(process.env.SLACK_ALERT_TOKEN) {
-          const slackWeb = new WebClient(process.env.SLACK_ALERT_TOKEN);
-          slackWeb.chat.postMessage({ text: message, channel: process.env.SLACK_ALERT_CHANNEL });
-        }
-        console.error(message);
-        process.exit(1);
-      }
-    });
-  }
-  catch(e) {
-    var message = `Payout extrinsic failed on-chain submission for validator ${stash_alias} (${stash_account}) with error ${e.message}.`;
-    if(process.env.SLACK_ALERT_TOKEN) {
-      const slackWeb = new WebClient(process.env.SLACK_ALERT_TOKEN);
-      const res = (await slackWeb.chat.postMessage({ text: message, channel: process.env.SLACK_ALERT_CHANNEL }));
+    if (claimed_eras.includes(eraToClaim)) {
+      console.log(`Payout for validator stash ${stash_account} for era ${eraToClaim} has already been issued`);
+      continue;
     }
-    console.error(message);
-    console.log(e);
-    process.exit(1);
+  
+    var exposure_for_era = await api.query.staking.erasStakers(eraToClaim, stash_account);
+    if (exposure_for_era.total == 0) {
+      console.log(`Stash ${stash_account} was not in the active validator set for era ${eraToClaim}, no payout can be made`);
+      continue;
+    }
+    console.log(`Issuing payoutStakers extrinsic from address ${payoutKey.address} for validator stash ${stash_alias} (${stash_account}) for era ${eraToClaim}`);
+  
+    // Create, sign and send the payoutStakers extrinsic
+    try {
+      var unsub = await api.tx.staking.payoutStakers(stash_account, eraToClaim).signAndSend(payoutKey, ({ events = [], status }) => {
+        console.log('Transaction status:', status.type);
+  
+        if (status.isInBlock) {
+          console.log('Included at block hash', status.asInBlock.toHex());
+          console.log('Events:');
+  
+          events.forEach(({ event: { data, method, section }, phase }) => {
+            console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
+          });
+        } else if (status.isFinalized) {
+          console.log('Finalized block hash', status.asFinalized.toHex());
+        } else if (status.isError) {
+          var message = `Payout extrinsic was succesfully finalized on-chain but failed for validator ${stash_alias} (${stash_account}) with error ${status.asFinalized.toHex()}.`;
+          if(process.env.SLACK_ALERT_TOKEN) {
+            const slackWeb = new WebClient(process.env.SLACK_ALERT_TOKEN);
+            slackWeb.chat.postMessage({ text: message, channel: process.env.SLACK_ALERT_CHANNEL });
+          }
+          console.error(message);
+          process.exit(1);
+        }
+      });
+    }
+    catch(e) {
+      var message = `Payout extrinsic failed on-chain submission for validator ${stash_alias} (${stash_account}) with error ${e.message}.`;
+      if(process.env.SLACK_ALERT_TOKEN) {
+        const slackWeb = new WebClient(process.env.SLACK_ALERT_TOKEN);
+        const res = (await slackWeb.chat.postMessage({ text: message, channel: process.env.SLACK_ALERT_CHANNEL }));
+      }
+      console.error(message);
+      console.log(e);
+      process.exit(1);
+    }
   }
   console.log("Exiting");
   process.exit(0);
